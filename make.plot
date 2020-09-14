080cc124e601c4ded7f9d8e86d6d11a061a56e26
Jon Takagi <jontakagi@gyoza.reed.edu>
2020-09-13 21:08:13 -0700

    first commit

---
+++ make.plot
@@ -0,0 +1,3 @@
+set term png enhanced giant size 1980,1080
+set output 'graph2.png'
+plot 'results.dat' with lines
---
+++ timer.c
@@ -0,0 +1,11 @@
+#include <sys/time.h>
+#include <stdio.h>
+int main() {
+    struct timeval time1;
+
+    for(int i = 0; i < 100; i++) {
+        gettimeofday(&time1, NULL);
+        printf("%ld\n", (time1.tv_usec));
+    }
+    return 0;
+}
